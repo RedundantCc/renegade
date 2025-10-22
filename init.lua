8ea399e070028344553fd9c018ec55f58ee2b797
RedundantCc <46980458+RedundantCc@users.noreply.github.com>
2025-10-22 15:20:41 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,2 +1,3 @@
+--run as: lua ../renegade/init.lua
 local modpath = minetest and core.get_modpath(core.get_current_modname()) or debug.getinfo(1, "S").source:gsub("^@", ""):match("^(.*)[/\\][^/\\]*$")
 dofile(modpath .. (DIR_DELIM or (modpath:match("[\\/]") or "/")) ..((minetest and core.get_current_modname() or modpath:match("[^\\/]+$"))..".lua"))
