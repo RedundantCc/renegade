f9800396b08fd40faa0d5d47092b8781306bdff1
RedundantCc <46980458+RedundantCc@users.noreply.github.com>
2025-10-23 19:26:27 -0400

    Bugfix: no return, initV7.lua

--- .init/initV7.lua
+++ .init/initV7.lua
@@ -1,3 +1,3 @@
---run as: lua ../renegade/init.lua
+--run as: lua ../modname/init.lua
 local modpath = minetest and core.get_modpath(core.get_current_modname()) or debug.getinfo(1, "S").source:gsub("^@", ""):match("^(.*)[/\\][^/\\]*$")
 return dofile(modpath .. (DIR_DELIM or (modpath:match("[\\/]") or "/")) ..((minetest and core.get_current_modname() or modpath:match("[^\\/]+$"))..".lua"))
